acd0f24d1314e8303744b2f0820c7995b7dd013d
karyRmartinez <karymartinez@pursuit.org>
2023-12-14 12:00:37 -0500

    Refactoring code creating DataMAnager class and 2 Models

---
+++ Sakila-Movies-Database/src/main/java/org/example/Actor.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Actor {
+    private int actorId;
+    private String firstName;
+    private String lastName;
+
+    public Actor(int actorId, String firstName, String lastName) {
+        this.actorId = actorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Getters and setters...
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "actorId=" + actorId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
